5a0ce1400303b6a51bf6c55f802744738c428122
Eric <erickanarr@gmail.com>
2017-06-30 09:39:53 -0400

    2nd commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+console.log("hola");
 // Require dependencies.
 const express = require('express');
 const parseurl = require('parseurl');
@@ -19,6 +20,10 @@ app.set('view engine', 'mustache');
 app.use(expressValidator());
 app.use(express.static('public'));
 
+// app.get('/', function(req, res){
+//   res.render('gameplay', { wrongLetter: })
+// })
+
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
@@ -28,50 +33,54 @@ app.use(session({
 // Instantiate array.
 const wrongLetter = [];
 const correctLetter = [];
-const mysteryWord = words(math.floor(math.random() * words.length));
+// const mysteryWord = words(Math.floor(Math.random() * words.length));
 
 // variables
 let startButton = document.getElementById('startButton');
-let submitButton = document.getElementById('submitButton');
-let letterGuessed = "";
-let searchArea = document.getElementById('searchArea');
+// let submitButton = document.getElementById('submitButton');
+// let letterGuessed = "";
+// let searchArea = document.getElementById('searchArea');
 
 
 // create button to start game and get random word
-function playGame() {
-  startButton.addEventListener("click", startButton);
+function playGame(array) {
+  const mysteryValue = array[Math.floor(Math.random() * array.length)];
+  return mysteryValue;
+  }
+  var mysteryWord = playGame(words);
+  // startButton.addEventListener("click", startGame);
+  // startButton.onclick = startGame;
   console.log(startButton);
   for (var i = 0; i < mysteryWord.length; i++) {
   mysteryWord.push('_');
 }
-
-// what to do with the guess by user
-function userGuess() {
-  submitButton.addEventListener("click", submitButton);
-  let letterGuessed = searchArea.value;
-  console.log("this is the letter guessed" + letterGuessed);
-  if (letterGuessed === " ") {
-    return("Please enter a letter");
-  }
-  else if (letterGuessed != "a" || "b" || "c" || "d" || "e" || "f" || "g" ||
-            "h" || "i" || "j" || "k" || "l" || "m" || "n" || "o" || "p" ||
-            "q" || "r" || "s" || "t" || "u" || "v" || "w" || "x" || "y" || "z") {
-    return("There are only letters in this word");
-  }
-  else if (letterGuessed.length != 1) {
-    return("One letter at a time");
-  }
-  else if (letterGuessed === correctLetter || letterGuessed === wrongLetter) {
-    return("Duplicate, try again");
-  }
-  else if (letterGuessed === mysteryWord) {
-    correctLetter.push('letterGuessed');
-  }
-  else (letterGuessed != mysteryWord ) {
-    wrongLetter.push('letterGuessed');
-  }
-}
-
-app.listen(3000, function (req, res) {
-  console.log('Successfully started express application!');
-});
+//
+// // what to do with the guess by user
+// function userGuess() {
+//   submitButton.addEventListener("click", submitButton);
+//   let letterGuessed = searchArea.value;
+//   console.log("this is the letter guessed" + letterGuessed);
+//   if (letterGuessed === " ") {
+//     return("Please enter a letter");
+//   }
+//   else if (letterGuessed != "a" || "b" || "c" || "d" || "e" || "f" || "g" ||
+//             "h" || "i" || "j" || "k" || "l" || "m" || "n" || "o" || "p" ||
+//             "q" || "r" || "s" || "t" || "u" || "v" || "w" || "x" || "y" || "z") {
+//     return("There are only letters in this word");
+//   }
+//   else if (letterGuessed.length != 1) {
+//     return("One letter at a time");
+//   }
+//   else if (letterGuessed === correctLetter || letterGuessed === wrongLetter) {
+//     return("Duplicate, try again");
+//   }
+//   else if (letterGuessed === mysteryWord) {
+//     correctLetter.push('letterGuessed');
+//   }
+//   // else (letterGuessed != mysteryWord ) {
+//   //   wrongLetter.push('letterGuessed');
+//   // }
+// // }
+//
+// app.listen(3000, function (req, res) {
+//   console.log('Successfully started express application!')
